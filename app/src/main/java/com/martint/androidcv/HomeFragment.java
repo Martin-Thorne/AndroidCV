7612813fb9ddf013e34a368891d4a6d7ff7a84ef
marti <martinjthorne@gmail.com>
2019-05-24 14:58:39 +0100

    Add text, with clickable link, to home page

--- app/src/main/java/com/martint/androidcv/HomeFragment.java
+++ app/src/main/java/com/martint/androidcv/HomeFragment.java
@@ -3,9 +3,11 @@ package com.martint.androidcv;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -23,7 +25,13 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Make links in fragments TextView clickable
+        TextView textView = view.findViewById(R.id.homeTextView);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        return view;
     }
 
 }
