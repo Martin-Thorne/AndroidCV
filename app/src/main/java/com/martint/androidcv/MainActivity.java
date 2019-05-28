d10b92f76e4cb1b74e12105f6a6aad65597b7ac3
marti <martinjthorne@gmail.com>
2019-05-28 10:31:33 +0100

    Add navigation drawer

--- app/src/main/java/com/martint/androidcv/MainActivity.java
+++ app/src/main/java/com/martint/androidcv/MainActivity.java
@@ -2,6 +2,8 @@ package com.martint.androidcv;
 
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -15,6 +17,13 @@ public class MainActivity extends AppCompatActivity {
         //Set up toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Set up navigation drawer
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         // Sets up view pager to allow the swiping between the fragments
         ViewPager viewPager = findViewById(R.id.viewPager);
