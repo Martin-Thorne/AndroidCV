7158fde5baecec18b8231911f00692c56dae9a2d
marti <martinjthorne@gmail.com>
2019-05-28 09:09:47 +0100

    Set up toolbar

--- app/src/main/java/com/martint/androidcv/MainActivity.java
+++ app/src/main/java/com/martint/androidcv/MainActivity.java
@@ -3,6 +3,7 @@ package com.martint.androidcv;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set up toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         // Sets up view pager to allow the swiping between the fragments
         ViewPager viewPager = findViewById(R.id.viewPager);
